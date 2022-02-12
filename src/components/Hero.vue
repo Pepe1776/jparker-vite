2b045d64c6d7278db45df5c0843d4c6441e18e38
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-02-12 12:41:42 -0700

    initial

---
+++ src/components/Hero.vue
@@ -0,0 +1,28 @@
+<template>
+  <!--Hero-->
+  <div
+    class="py-20"
+    style="background: linear-gradient(90deg, #c4cffd 0%, #4b8ea2 100%)"
+  >
+    <div class="container mx-auto px-6">
+      <h2 class="text-4xl font-bold mb-2 text-white">
+        Newest and Fastest technology available
+      </h2>
+      <h3 class="text-2xl mb-8 text-gray-200">
+        bring your business to the public at light speed.
+      </h3>
+      <button
+        id="learn"
+        class="text-cyan-500 bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider"
+      >
+        Ignite
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+</script>
+
+<style>
+</style>
