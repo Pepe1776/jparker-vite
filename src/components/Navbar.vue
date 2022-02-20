16a3ad3c28a7f4b4d27bbdd8f7becb4cc588b3e6
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-02-19 17:23:27 -0700

    nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,10 +15,42 @@
         ><button
           class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none"
           type="button"
-          onclick="toggleNavbar('example-collapse-navbar')"
+          @click="toggleNavbar = !toggleNavbar"
         >
-          <span class="text-indigo-500 lg:hidden">=</span>
+          <span class="text-white lg:hidden absolute top-3 right-0">=</span>
         </button>
+      </div>
+      <div class="toggle" :class="toggleNavbar ? 'flex' : 'hidden'">
+        <ul class="flex justify-center">
+          <!-- Icon 1 -->
+          <li class="flex items-center justify-center">
+            <a
+              id="learn"
+          class="nter hover:text-indigo-300 text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
+              href="https://www.instagram.com/j_not_query"
+              ><span class="hover:text-indigo-300 inline-block ml-2">Instagram</span></a
+            >
+          </li>
+          <!-- Icon 2 -->
+          <li class="flex items-center">
+            <a
+                id="learn"
+          class="nter hover:text-indigo-300 text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
+              href="https://www.github.com/pepe1776"
+              >
+              <span class="lato hover:text-indigo-300 inline-block ml-2">GitHub</span></a
+            >
+          </li>
+          <!-- Icon 3 -->
+          <li class="flex items-center">
+            <a
+               id="learn"
+          class="nter hover:text-indigo-300 text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
+              href="mailto:justinp@aevio.com"
+              ><span class="hover:text-indigo-300inline-block ml-2">Email</span></a
+            >
+          </li>
+        </ul>
       </div>
       <div
         class="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none hidden"
@@ -60,7 +92,13 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      toggleNavbar: false,
+    };
+  },
+};
 </script>
 
 <style>
